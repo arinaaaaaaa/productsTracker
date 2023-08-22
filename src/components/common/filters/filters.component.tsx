e2aa127af6bd920246e6ddbb368c7cf186159d64
arina krumm <92879566+arinaaaaaaa@users.noreply.github.com>
2023-08-22 17:06:19 +0300

    Добавлен компонент фильтра по категориям

--- src/components/common/filters/filters.component.tsx
+++ src/components/common/filters/filters.component.tsx
@@ -1,22 +1,42 @@
 import styles from './filters.module.scss';
+import { useContext, useState } from 'react';
+import { DataContext } from '../../pages/home.page';
 
 export default function FiltersComponent() {
+    const data = useContext(DataContext);
+    const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(null);
+  
+    function getCounter(categoryId: number): number {
+        let counter: number = 0;
+
+        // Подсчет продуктов принадлежащих категории
+        data.data.products.forEach((product) => 
+            product.category == categoryId ? counter ++ : null
+        )
+
+        return counter;
+    }
+
+    function chooseFilter(categoryId: number) {
+        if (selectedCategoryId !== categoryId) setSelectedCategoryId(categoryId)
+        else setSelectedCategoryId(null)
+    }
+  
     return (
-        <div className={styles.filtersWrapper}>
-            <div className={styles.categoriesList}>
-                <div className={styles.category}>
-                    <p className={styles.name}>суши</p>
-                    <p className={styles.counter}>1</p>
-                </div>
-                <div className={styles.category}>
-                    <p className={styles.name}>пицца</p>
-                    <p className={styles.counter}>1</p>
-                </div>
-                <div className={styles.category}>
-                    <p className={styles.name}>бургеры</p>
-                    <p className={styles.counter}>2</p>
-                </div>
+      <div className={styles.filtersWrapper}>
+        <div className={styles.categoriesList}>
+          {data.data.categories.map((category) => (
+            <div
+                className={`${styles.category} ${selectedCategoryId === category.id ? styles.active : ''}`}
+                key={category.id}
+                style={{ backgroundColor: category.color }}
+                onClick={() => chooseFilter(category.id)}
+            >
+              <p className={styles.name}>{category.name}</p>
+              <p className={styles.counter}>{getCounter(category.id)}</p>
             </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    );
+  }
