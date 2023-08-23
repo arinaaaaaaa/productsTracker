87489353d27795c35f90583eef73ed9798985278
arina krumm <92879566+arinaaaaaaa@users.noreply.github.com>
2023-08-23 12:32:44 +0300

    Закрытие выпадающего списка при нажатии вне dropdown

--- src/core/abstract.components/dropdown/dropdown.component.tsx
+++ src/core/abstract.components/dropdown/dropdown.component.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, useRef } from 'react';
 import { DataContext } from '../../../components/pages/home.page';
 import { Sort } from '../../interfaces/sort.interface';
 import styles from "./dropdown.module.scss";
@@ -6,24 +6,42 @@ import styles from "./dropdown.module.scss";
 export default function DropdownComponent({ sortList, selectedCategory }: { sortList: Sort[], selectedCategory: number | null }) {
     const {setShownData} = useContext(DataContext);
     const data = useContext(DataContext);
+
+    const dropdownRef = useRef<any | null>(null);
     
     const [isOpen, setIsOpen] = useState(true);
     const [sort, setSort] = useState<Sort | null>(null);
 
+    //Если выбрана другая категория, обнуляется сортировка
     useEffect(() => setSort(null), [selectedCategory])
+
+    //Если пользователь кликнул вне dropdown, закрыть выпадающий список
+    useEffect(() => {
+        document.addEventListener("click", handleOutsideClick);
+        return () => {
+          document.removeEventListener("click", handleOutsideClick);
+        };
+    }, []);
+
+    //Функция закрытия списка (установка значения false для isOpen)
+    function handleOutsideClick(event: any) {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target))
+            setIsOpen(false);
+    };
 
     //Сортировка отображаемых продуктов по дате
     function sortData(sortItem: Sort) {
         //Если выбран не ранее используемая сортировка
         if (!sort || sortItem.id !== sort.id) {
             setSort(sortItem);
-      
+            
             setShownData((prevShownData) => {
-            const sortedByDate = prevShownData.slice().sort((productA, productB) => {
-                if (productA.date < productB.date) return (-1 * sortItem.order);
-                if (productA.date > productB.date) return (1 * sortItem.order);
-                return 0;
-            });
+                const sortedByDate = prevShownData.slice().sort((productA, productB) => {
+                    if (productA.date < productB.date) return (-1 * sortItem.order);
+                    if (productA.date > productB.date) return (1 * sortItem.order);
+                    return 0;
+                }
+            );
 
             return sortedByDate;
             });
@@ -36,7 +54,7 @@ export default function DropdownComponent({ sortList, selectedCategory }: { sort
       }
 
     return (
-        <div className={styles.dropdownMenu}>
+        <div className={styles.dropdownMenu} ref={dropdownRef}>
             <div className={styles.dropdownHeader} onClick={() => setIsOpen(!isOpen)}>
                 <p>{sort ? sort.title : 'отсортировать'}</p>
                 <img src="images/chevron.png" alt="" />
